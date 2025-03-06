016e53b8b95707e1f97b4d8ceadd319daaa27e1d
yashweblife <yashweblife20@gmail.com>
2025-03-06 12:42:38 -0600

    implement store

--- components/homepage/EatOutModal.tsx
+++ components/homepage/EatOutModal.tsx
@@ -1,17 +1,22 @@
+import { HomeContext } from "@/store/homeProvider";
+import { useContext } from "react";
 import { View, } from "react-native";
 import { Button, Modal, Portal, Text, useTheme } from "react-native-paper";
 
-export type EatOutModalProps = {
-    state: boolean,
-    setState: (state: boolean) => void
-    handleSubmit: (val: number) => void
-}
-
-export default function EatOutModal({ state, setState, handleSubmit }: EatOutModalProps) {
+export default function EatOutModal() {
     const { colors } = useTheme();
+    const {
+        eatOutModalOpen,
+        closeModal,
+        decreasePoints
+    } = useContext(HomeContext);
+    const handleSelection = (val:number)=>{
+        decreasePoints(val);
+        closeModal();
+    }
     return (
         <Portal>
-            <Modal visible={state} onDismiss={() => setState(false)}
+            <Modal visible={eatOutModalOpen} onDismiss={() => closeModal()}
                 contentContainerStyle={{
                     padding: 20
                 }}
@@ -32,24 +37,20 @@ export default function EatOutModal({ state, setState, handleSubmit }: EatOutMod
                     </View>
                     <View style={{ flexDirection: "column", gap: 10, padding: 10 }}>
                         <Button onPress={() => {
-                            handleSubmit(10);
-                            setState(false);
+                            handleSelection(10);
                         }} mode="outlined">Fast Food</Button>
                         <Button onPress={() => {
-                            handleSubmit(10);
-                            setState(false);
+                            handleSelection(10);
                         }} mode="outlined">Budget Restaurant</Button>
                         <Button onPress={() => {
-                            handleSubmit(10);
-                            setState(false);
+                            handleSelection(10);
                         }} mode="outlined">Expensive Restaurant</Button>
                         <Button onPress={() => {
-                            handleSubmit(10);
-                            setState(false);
+                            handleSelection(10);
                         }} mode="outlined">Other</Button>
                     </View>
 
-                    <Button mode="contained" onPress={() => setState(false)}>Close</Button>
+                    <Button mode="contained" onPress={() => closeModal()}>Close</Button>
                 </View>
             </Modal>
         </Portal>
