7598fbc882bbe1401953b59e1d955615340b00b4
yashweblife <yashweblife20@gmail.com>
2025-03-06 12:44:17 -0600

    new settings page

---
+++ app/settings.tsx
@@ -0,0 +1,9 @@
+import { BaseView } from "@/components/common";
+import { Text } from "react-native-paper";
+export default function Settings(){
+    return(
+        <BaseView name="Settings">
+            <Text>Settings</Text>
+        </BaseView>
+    )
+}
