05d5360d72a4d9855ec0f08453f1968f71123f7c
yashweblife <yashweblife20@gmail.com>
2025-03-06 12:44:02 -0600

    use custom theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,25 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { MD2DarkTheme, PaperProvider } from "react-native-paper";
 
+const customTheme = {
+    ...MD2DarkTheme,
+    colors: {
+        ...MD2DarkTheme.colors,
+        surface: '#2A2A2A',
+        primary: '#659765',
+    }
+}
+
+
 export default function RootLayout() {
     return (
-        <PaperProvider theme={MD2DarkTheme}>
+        <PaperProvider theme={customTheme}>
             <Stack>
                 <Stack.Screen name="index" />
                 <Stack.Screen name="dashboard" />
                 <Stack.Screen name="+not-found" />
             </Stack>
-            <StatusBar/>
+            <StatusBar />
         </PaperProvider>
     )
 }
