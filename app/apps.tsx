b624685c3cfb1cc6d47cefb150c8753e92c3e078
yashweblife <yashweblife20@gmail.com>
2025-03-06 12:44:10 -0600

    new page

---
+++ app/apps.tsx
@@ -0,0 +1,9 @@
+import { BaseView } from "@/components/common";
+import { Text } from "react-native-paper";
+export default function AppsPage(){
+    return(
+        <BaseView name="Apps">
+            <Text>Apps</Text>
+        </BaseView>
+    )
+}
