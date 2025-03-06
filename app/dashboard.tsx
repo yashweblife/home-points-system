bbbbe285e72a3533246bd056030706ff27110298
yashweblife <yashweblife20@gmail.com>
2025-03-05 20:39:57 -0600

    first pages

---
+++ app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { BaseView } from "@/components/common";
+import { Text } from "react-native-paper";
+
+export default function Dashboard(){
+    return(
+        <BaseView name="dashboard">
+            <Text>Dashboard</Text>
+        </BaseView>
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { BaseView } from "@/components/common";
+import { Text } from "react-native-paper";
+
+export default function HomePage(){
+    return(
+        <BaseView name="Home">
+            <Text>Home Page</Text>
+        </BaseView>
+    )
+}
